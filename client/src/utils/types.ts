cd4b256c0fa7cb716520502e0865ea6ba9ca9d08
Prayansh Srivastava <prayansh@gmail.com>
2020-05-17 14:08:41 -0700

    add tool interfaces

--- client/src/utils/types.ts
+++ client/src/utils/types.ts
@@ -1,4 +1,12 @@
-import { TYPE_HOST_ROOM, TYPE_JOIN_ROOM } from './consts'
+import {
+  TOOL_ICON, TOOL_LINE,
+  TOOL_PEN,
+  TOOL_SHAPE,
+  TOOL_TEXT,
+  TYPE_HOST_ROOM,
+  TYPE_JOIN_ROOM,
+  TYPE_TOOL
+} from './consts'
 
 export enum RoomType {
   SIEGE,
@@ -6,6 +14,8 @@ export enum RoomType {
   CSGO,
   RL
 }
+
+/** Socket Messages **/
 
 // Message sent over the wire
 export interface HostRoomMessage {
@@ -26,4 +36,75 @@ export interface JoinRoomMessage {
   }
 }
 
-export type Message = HostRoomMessage | JoinRoomMessage
+type Action = 'add' | 'edit' | 'delete'
+
+export interface ToolMessage {
+  type: typeof TYPE_TOOL
+  action: Action
+  payload: Drawable
+}
+
+export type Message = HostRoomMessage | JoinRoomMessage | ToolMessage
+
+/** Whiteboard Tools **/
+
+type Color = string
+
+type Point = {
+  x: number
+  y: number
+}
+
+interface Drawable {
+  color: Color
+  scaleX: number
+  scaleY: number
+
+  draw(ctx: CanvasRenderingContext2D): void
+}
+
+interface Draggable {
+  highlighted: boolean
+
+  contains(pt: Point): boolean
+}
+
+interface Rotatable {
+  rotation: number
+
+  rotate(ctx: CanvasRenderingContext2D): void
+}
+
+export interface PenTool extends Drawable, Draggable {
+  type: typeof TOOL_PEN
+  points: Point[]
+}
+
+export interface TextTool extends Drawable, Draggable, Rotatable {
+  type: typeof TOOL_TEXT
+  text: string
+  origin: Point // Top-left
+}
+
+export interface IconTool extends Drawable, Draggable {
+  type: typeof TOOL_ICON
+  src: string
+  origin: Point // Top-left
+}
+
+type Shape = 'rectangle' | 'circle' | 'triangle'
+
+export interface ShapeTool extends Drawable, Draggable, Rotatable {
+  type: typeof TOOL_SHAPE
+  shapeName: Shape
+  origin: Point // Top-left
+  height: number
+  width: number
+}
+
+export interface LineTool extends Drawable, Draggable, Rotatable {
+  type: typeof TOOL_LINE
+  hasArrowHead: boolean
+  origin: Point
+  end: Point
+}
