4c37f930d7f3af2da11f23e783a031c1258b315b
Prayansh Srivastava <prayansh@users.noreply.github.com>
2020-06-06 13:14:32 -0700

    add tool interfaces (#5)
    * add tool interfaces
    
    * update types
    
    * fix types

--- client/src/utils/consts.ts
+++ client/src/utils/consts.ts
@@ -2,3 +2,10 @@
 export const TYPE_JOIN_ROOM = 'joinRoom'
 export const TYPE_HOST_ROOM = 'hostRoom'
 export const TYPE_INVALID_ROOM = 'invalidRoom'
+export const TYPE_TOOL = 'tool'
+
+export const TOOL_PEN = 'pen'
+export const TOOL_TEXT = 'text'
+export const TOOL_ICON = 'icon'
+export const TOOL_SHAPE = 'shape'
+export const TOOL_LINE = 'line'
--- client/src/utils/types.ts
+++ client/src/utils/types.ts
@@ -1,4 +1,12 @@
-import { TYPE_HOST_ROOM, TYPE_JOIN_ROOM } from './consts'
+import {
+  TOOL_ICON, TOOL_LINE,
+  TOOL_PEN,
+  TOOL_SHAPE,
+  TOOL_TEXT,
+  TYPE_HOST_ROOM,
+  TYPE_JOIN_ROOM,
+  TYPE_TOOL
+} from './consts'
 
 export enum RoomType {
   SIEGE,
@@ -6,6 +14,8 @@ export enum RoomType {
   CSGO,
   RL
 }
+
+/** Socket Messages **/
 
 // Message sent over the wire
 export interface HostRoomMessage {
@@ -26,4 +36,74 @@ export interface JoinRoomMessage {
   }
 }
 
-export type Message = HostRoomMessage | JoinRoomMessage
+type Action = 'add' | 'edit' | 'delete'
+
+export interface ToolMessage {
+  id?: string
+  type: typeof TYPE_TOOL
+  action: Action
+  payload: Drawable
+}
+
+export type Message = HostRoomMessage | JoinRoomMessage | ToolMessage
+
+/** Whiteboard Tools **/
+
+type Color = string
+
+type Point = {
+  x: number
+  y: number
+}
+
+interface Drawable {
+  origin: Point
+  color: Color
+  scaleX: number
+  scaleY: number
+
+  draw(ctx: CanvasRenderingContext2D): void
+}
+
+interface Draggable {
+  highlighted: boolean
+
+  contains(pt: Point): boolean
+}
+
+interface Rotatable {
+  rotation: number
+
+  rotate(ctx: CanvasRenderingContext2D): void
+}
+
+export interface PenTool extends Drawable, Draggable {
+  type: typeof TOOL_PEN
+  points: Point[] // all except origin point
+}
+
+export interface TextTool extends Drawable, Draggable, Rotatable {
+  type: typeof TOOL_TEXT
+  text: string
+  size: number
+}
+
+export interface IconTool extends Drawable, Draggable {
+  type: typeof TOOL_ICON
+  src: string
+}
+
+type Shape = 'rectangle' | 'circle' | 'triangle'
+
+export interface ShapeTool extends Drawable, Draggable, Rotatable {
+  type: typeof TOOL_SHAPE
+  shapeName: Shape
+  height: number
+  width: number
+}
+
+export interface LineTool extends Drawable, Draggable, Rotatable {
+  type: typeof TOOL_LINE
+  hasArrowHead: boolean
+  end: Point
+}
